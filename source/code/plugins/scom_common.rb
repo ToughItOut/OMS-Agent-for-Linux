312f8c7e6f1a0d0e9780ef32cc1db40e38659f9e
Laura Galbraith <lagalbra@microsoft.com>
2017-03-02 11:03:35 -0800

    Remove VERIFY_NONE from SSL verification process

--- source/code/plugins/scom_common.rb
+++ source/code/plugins/scom_common.rb
@@ -66,7 +66,6 @@ module SCOM
     def self.create_secure_http(uri)
       http = Net::HTTP.new( uri.host, uri.port )
       http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.open_timeout = 30
       return http
     end
